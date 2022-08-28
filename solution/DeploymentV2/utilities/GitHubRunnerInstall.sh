9d32a558966f0826c974523931350d098dd753c1
jorampon@microsoft.com <jorampon@microsoft.com>
2022-08-28 12:53:59 +1000

    Updated Terraform Lock

--- solution/DeploymentV2/utilities/GitHubRunnerInstall.sh
+++ solution/DeploymentV2/utilities/GitHubRunnerInstall.sh
@@ -6,7 +6,7 @@ rm ./powershell-lts_7.2.5-1.deb_amd64.deb  && \
 sudo apt install -y dotnet-sdk-6.0  && \
 
 #May need to do below on ubuntu 20.04
-sudo apt install -y aspnetcore-runtime-6.0=6.0.8-1 dotnet-apphost-pack-6.0=6.0.8-1 dotnet-host=6.0.8-1 dotnet-hostfxr-6.0=6.0.8-1 dotnet-runtime-6.0=6.0.8-1 dotnet-sdk-6.0=6.0.400-1 dotnet-targeting-pack-6.0=6.0.8-1
+#sudo apt install -y aspnetcore-runtime-6.0=6.0.8-1 dotnet-apphost-pack-6.0=6.0.8-1 dotnet-host=6.0.8-1 dotnet-hostfxr-6.0=6.0.8-1 dotnet-runtime-6.0=6.0.8-1 dotnet-sdk-6.0=6.0.400-1 dotnet-targeting-pack-6.0=6.0.8-1
 
 wget https://github.com/google/go-jsonnet/releases/download/v0.17.0/jsonnet-go_0.17.0_linux_amd64.deb  && \
 sudo dpkg -i jsonnet-go_0.17.0_linux_amd64.deb && \
@@ -19,3 +19,14 @@ sudo mv terragrunt_linux_amd64 terragrunt && \
 sudo chmod u+x terragrunt && \
 sudo mv terragrunt /usr/local/bin/terragrunt && \
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+#Github Runner Software
+mkdir actions-runner && cd actions-runner# Download the latest runner package && \
+curl -o actions-runner-linux-x64-2.296.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.0/actions-runner-linux-x64-2.296.0.tar.gz && \
+tar xzf ./actions-runner-linux-x64-2.296.0.tar.gz && \
+read -p "Please enter github runner token: " GHTOKEN && \
+read -p "Please enter github repo url eg. https://github.com/microsoft/azure-data-services-go-fast-codebase  " GHURL && \
+./config.sh --url $GHURL --token $GHTOKEN && \
+rm actions-runner-linux-x64-2.296.0.tar.gz && \
+sudo ./svc.sh install && \ 
+sudo ./svc.sh start
