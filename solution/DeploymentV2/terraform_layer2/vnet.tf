cacbffa8dcc05569d1e6bca815f91d182df42170
h-sha <97069267+h-sha@users.noreply.github.com>
2022-10-27 14:00:14 +1100

    adding vnet config

--- solution/DeploymentV2/terraform_layer2/vnet.tf
+++ solution/DeploymentV2/terraform_layer2/vnet.tf
@@ -1,6 +1,11 @@
 
+locals {
+  vnet_id = (var.existing_vnet_id == "" && (var.is_vnet_isolated) ? azurerm_virtual_network.vnet[0].id : var.existing_vnet_id)
+  vnet_name = (var.existing_vnet_name == "" && (var.is_vnet_isolated) ? azurerm_virtual_network.vnet[0].name : var.existing_vnet_name)
+}
+
 resource "azurerm_virtual_network" "vnet" {
-  count               = (var.is_vnet_isolated || var.deploy_selfhostedsql || var.deploy_h2o-ai ? 1 : 0)
+  count               = ((var.is_vnet_isolated || var.deploy_selfhostedsql || var.deploy_h2o-ai) && var.existing_vnet_id == "" ? 1 : 0)
   name                = local.vnet_name
   location            = var.resource_location
   resource_group_name = var.resource_group_name
